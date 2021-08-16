1a18a4de34f8fbdbdbb8de76e353e2672759b213
RODRIGUEZ CASTRO Nicolás Agustín <RodriguezN@sancristobal.com.ar>
2021-08-15 21:37:01 -0300

    elements module with the Number class, an implementation of inmutable real numbers.

---
+++ src/data_structures/__init__.py
@@ -0,0 +1,13 @@
+"""Some useful data structures for problem solving.
+
+Modules
+-------
+
+elements:
+    Basic elements to use in data structures.
+
+    Classes
+    -------
+    Number:
+        Immutable real numbers.
+"""
